defa6c409cc30f848005f065c131987d1af5918c
pjsdevcom <mail@pjsdev.com>
2021-06-08 10:04:57 +0100

    Add test configured with kebab case property naming strategy

--- src/test/java/com/pjsdev/msscjacksonexamples/model/BaseTest.java
+++ src/test/java/com/pjsdev/msscjacksonexamples/model/BaseTest.java
@@ -1,10 +1,19 @@
 package com.pjsdev.msscjacksonexamples.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@JsonTest
 public class BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     BeerDto getDto() {
         return BeerDto.builder()
                 .beerName("Top Bloke")
